0d654c6735c7124be6fcfd5f9619a1cd85840317
LuisDevLipe <luisfelipebsj@gmail.com>
2024-09-15 02:24:45 -0300

    feat: menu responsivo

---
+++ components/navbar/scripts.js
@@ -0,0 +1,29 @@
+const navbar = document.querySelector('.main-navigation');
+console.log(navbar)
+const toggle = document.querySelector('.toggle');
+console.log(toggle)
+
+window.matchMedia('(max-width: 768px)').addEventListener('change', (e) => {
+    if (e.matches) {
+        navbar.classList.add('mobile');
+    } else {
+        navbar.classList.remove('mobile');
+    }
+});
+
+window.addEventListener('resize',() => {
+    if (window.innerWidth < 768) {
+        navbar.classList.add('mobile');
+    }
+    else {
+        navbar.classList.remove('mobile');
+    }
+})
+
+toggle.addEventListener('click', () => {
+    if(navbar.classList.contains('closed')) {
+        navbar.classList.replace('closed', 'opened');
+    } else {
+        navbar.classList.replace('opened', 'closed');
+    }
+})
