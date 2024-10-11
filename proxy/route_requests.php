14076eca004fd737499e704c79d4b8f785c67405
LuisDevLipe <luisfelipebsj@gmail.com>
2024-10-11 05:45:41 -0300

    added: cadastro now has his own route/ address is now saved on a new register

--- proxy/route_requests.php
+++ proxy/route_requests.php
@@ -33,9 +33,9 @@ class Route_requests
                     $user = $credential->login();
                     $_POST = [];
                     if (!$user) {
-                       session_start();
+                        session_start();
                         $_SESSION['not_found_user'] = true;
-                        session_commit(); 
+                        session_commit();
                         header("Location: /jogodobicho/pages/login/login.php");
                         exit();
 
@@ -83,16 +83,27 @@ class Route_requests
                     $newTwoFAInstance = new \controllers\TwoFactorAuthController(user_id: $user_id, twoFaAnswer: $twoFaAnswer, twoFaAnswerId: $twoFaAnswerId);
                     $twoFaVerified = $newTwoFAInstance->verifyTwoFactorAuth();
 
-                    
+
                     if (!$twoFaVerified) {
                         header("Location: /jogodobicho/pages/auth-util/TwoFactorAuthentication.php");
                         exit();
                     }
-                    
-                    logNewSuccessfulAuth();
+
+                    if (session_status() === PHP_SESSION_NONE) {
+                        session_start();
+                        var_dump($_POST);
+                    }
+                    $username = $_SESSION["username"];
+                    $session_id = session_id();
+                    logNewSuccessfulAuth(
+                        username: $username,
+                        twoFaAnswer: $twoFaAnswer,
+                        session_id: $session_id
+                    );
+                    session_commit();
                     header("Location: /jogodobicho/");
                     exit();
-                    
+
                 }
                 break;
 
@@ -107,6 +118,74 @@ class Route_requests
                     header("Location: /jogodobicho/");
                     exit();
                 }
+                break;
+
+            case 'cadastro':
+
+                if (isset($_POST["cadastrar"])) {
+
+                    include_once $_SERVER["DOCUMENT_ROOT"] . "/jogodobicho/controllers/Credential.php";
+                    include_once $_SERVER["DOCUMENT_ROOT"] . "/jogodobicho/controllers/User.php";
+                    include_once $_SERVER["DOCUMENT_ROOT"] . "/jogodobicho/controllers/Address.php";
+
+                    $new_address = new \controllers\AdressController(
+                        cep: $_POST["cep"],
+                        logradouro: $_POST["logradouro"],
+                        numero: $_POST["numero"],
+                        cidade: $_POST["cidade"],
+                        estado: $_POST["estado"],
+                        complemento: $_POST["complemento"],
+                        bairro: $_POST["bairro"],
+                        pais: $_POST["pais"]
+                    );
+
+                    $address_id = $new_address->createAddressAndReturn_id();
+
+
+                    echo "<script>alert('Endereço cadastrado com sucesso')</script>";
+
+                    $newUser = new \controllers\UserController(
+                        fullname: $_POST["name"],
+                        dob: $_POST["dob"],
+                        gender: $_POST["gender"],
+                        mothername: $_POST["filiation-name"],
+                        cpf: $_POST["cpf"],
+                        email: $_POST["email"],
+                        celular: $_POST["celular"],
+                        fixo: $_POST["fixo"],
+                        address_id: $address_id
+                    );
+
+                    $userSaved = $newUser->registerUser();
+
+                    if (!$userSaved) {
+                        echo "<script>alert('CPF de Usuário já cadastrado')</script>";
+                        header("Location: /jogodobicho/pages/cadastro/cadastro.php");
+                        exit();
+                    }
+                    $newUser_id = $newUser->findUser()["ID"];
+
+
+                    $newUserCredentials = new \controllers\CredentialController(
+                        username: $_POST["username"],
+                        password: $_POST["password"]
+                    );
+
+                    $newUserCredentials->setUserId($newUser_id);
+
+                    $credentialSaved = $newUserCredentials->registerCredential();
+
+                    if (!$credentialSaved) {
+                        echo "<script>alert('Nome de Usuário já cadastrado')</script>";
+                        exit();
+                    }
+                    echo "<script>alert('Usuário cadastrado com sucesso')</script>";
+                    header("Location: /jogodobicho/pages/login/login.php");
+
+                    exit();
+
+                }
+                break;
 
 
         }
@@ -122,12 +201,15 @@ $uri = $_SERVER['REQUEST_METHOD'] == 'GET' ? $_GET["url"] : $_POST["url"];
 $router = new Route_requests($uri);
 $router->route();
 
-function logNewSuccessfulAuth(): void{
+function logNewSuccessfulAuth($username, $twoFaAnswer, $session_id): void
+{
     include_once $_SERVER['DOCUMENT_ROOT'] . "/jogodobicho/controllers/UserLog.php";
+    if (session_status() === PHP_SESSION_NONE) {
         session_start();
-        $userLog = new \controllers\UserLogController(username: $_SESSION["username"]);
-        $userLog->create();
-        session_commit();
+    }
+    $userLog = new \controllers\UserLogController(username: $username, twoFaAnswer: $twoFaAnswer, session_id: $session_id);
+    $userLog->create();
+    session_commit();
 }
 
 ?>
